e612c3a75b08b25e5db8348dd6febfa84b010971
ramus <ramussiz29@gmail.com>
2024-04-16 06:03:38 +0100

    feat rent-book: add contract page

--- carnet-de-loyer/src/pages/tenantsPages/my-agreement.jsx
+++ carnet-de-loyer/src/pages/tenantsPages/my-agreement.jsx
@@ -1,3 +1,32 @@
 export default function MyAgreement() {
-  return <div>Mon contrat</div>;
+  return (
+    <div className="p-8 ">
+      <div className="bg-white h-[40em] w-[35em] pt-8 ">
+        <h2 className="text-center text-xl pb-4 font-semibold">
+          CONTRAT DE BAIL
+        </h2>
+        <div className="p-8">
+          <h2 className="pb-4 font-semibold">ENTRE LES SOUSSIGNES,</h2>
+          <p>
+            Monsieur KABEYA TSHIKUNA PAX , résidant au N°3329 de L’avenue Du
+            carmel, quartier Gambela 2 dans la commune de Lubumbashi à
+            Lubumbashi
+          </p>
+          <p className="pt-4">Ci-après dénommé « Bailleur » d’une part ;</p>
+          <p className="pt-4 pb-4">Et</p>
+          <p> Monsieur/Madame/Mademoiselle …………………….</p>
+          <p className="pt-4">Ci-après dénommé « Locataire » d’autre part.</p>
+          <h2 className="pt-4 pb-4 text-center font-semibold">
+            IL A ETE CONVENU ET ARRETE CE QUI SUIT :
+          </h2>
+          <p>
+            <span className="font-semibold">Article 1 :</span> Le Bailleur donne
+            à titre de baille exclusivement résidentiel, au locataire qui
+            accepte dans l’état où il se trouve, la maison située au N°2918 de
+            l’avenue Kangele, Quartier Gambela 2 dans la commune Lubumbashi
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
