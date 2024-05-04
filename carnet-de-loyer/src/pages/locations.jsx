2bdbe4fc1571402c554c26dadcba25faf99dfb52
ramus <ramussiz29@gmail.com>
2024-05-04 16:46:33 +0100

    perf location: add spinner to the locations page

--- carnet-de-loyer/src/pages/locations.jsx
+++ carnet-de-loyer/src/pages/locations.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRentBooklet } from "../components/contexts/context";
 import Header from "../components/header";
 import axios from "axios";
+import Loader from "../components/loader";
 
 export default function Locations() {
   let tenants = useRentBooklet((state) => state.tenants);
@@ -12,6 +13,9 @@ export default function Locations() {
   let currentUser = useRentBooklet((state) => state.currentUser);
   const updateCurrentUser = useRentBooklet((state) => state.updateCurrentUser);
   const userUrl = `http://localhost:3000/my-tenants/lessor/${currentUser.lessorId}`;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const catalog = tenants.map((item, index) => {
     let background = "";
@@ -60,8 +64,12 @@ export default function Locations() {
       });
 
       updateTenants(data);
+      setData(data);
     } catch (error) {
       console.error("Erreur lors de la récupération des données:", error);
+      setError(error);
+    } finally {
+      setLoading(false);
     }
   };
   useEffect(() => {
@@ -69,35 +77,61 @@ export default function Locations() {
     getHouseData();
   }, [currentUser.lessorId, updateCurrentUser]);
 
-  return (
-    <>
-      <Header />
-      <div className="p-1">
-        <table className="border border-1 w-full text-[#b3b5b7] ">
-          <thead>
-            <tr className="bg-[#c299d0] text-white">
-              <th className="border border-1 border-[#5f6263] p-4">
-                Locataire
-              </th>
-              <th className="border border-1 border-[#5f6263] p-4">
-                Téléphone
-              </th>
-              <th className="border border-1  border-[#5f6263] p-4">Adresse</th>
-              <th className="border border-1 border-[#5f6263] p-4">Maison</th>
-              <th className="border border-1 border-[#5f6263] p-4">
-                Début du contrat
-              </th>
-              <th className="border border-1  border-[#5f6263] p-4">
-                Fin du contrat
-              </th>
-              <th className="border border-1 border-[#5f6263] p-4">État</th>
-            </tr>
-          </thead>
-          <tbody className="border border-1 border-[#5f6263]">{catalog}</tbody>
-        </table>
-      </div>
-    </>
-  );
+  if (loading) {
+    return (
+      <>
+        <Header />
+        <div className=" flex justify-center items-center h-[70%]">
+          <Loader />
+        </div>
+      </>
+    );
+  }
+  if (error) {
+    return (
+      <>
+        <Header />
+        <div className=" flex justify-center items-center h-[70%]">
+          <Loader />
+        </div>
+      </>
+    );
+  }
+  if (data) {
+    return (
+      <>
+        <Header />
+        <div className="p-1">
+          <table className="border border-1 w-full text-[#b3b5b7] ">
+            <thead>
+              <tr className="bg-[#c299d0] text-white">
+                <th className="border border-1 border-[#5f6263] p-4">
+                  Locataire
+                </th>
+                <th className="border border-1 border-[#5f6263] p-4">
+                  Téléphone
+                </th>
+                <th className="border border-1  border-[#5f6263] p-4">
+                  Adresse
+                </th>
+                <th className="border border-1 border-[#5f6263] p-4">Maison</th>
+                <th className="border border-1 border-[#5f6263] p-4">
+                  Début du contrat
+                </th>
+                <th className="border border-1  border-[#5f6263] p-4">
+                  Fin du contrat
+                </th>
+                <th className="border border-1 border-[#5f6263] p-4">État</th>
+              </tr>
+            </thead>
+            <tbody className="border border-1 border-[#5f6263]">
+              {catalog}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 }
 
 // {
