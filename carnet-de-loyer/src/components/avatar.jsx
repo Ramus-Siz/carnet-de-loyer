9fbac03ef1b9c1cfa992bd47c5505342533e9426
ramus <ramussiz29@gmail.com>
2024-05-10 18:55:13 +0100

    style avatar: put user icone"

--- carnet-de-loyer/src/components/avatar.jsx
+++ carnet-de-loyer/src/components/avatar.jsx
@@ -1,16 +1,17 @@
 export default function Avatar() {
+  const getCurrentUser = sessionStorage.getItem("currentUser");
+  const currentUser = JSON.parse(getCurrentUser);
   return (
     <>
       <div className=" flex justify-between items-center gap-10 p-4 w-full md:w-[14.8em] border-t-8 border-[#283342] md:fixed bottom-0">
-        <div className="flex items-center gap-4">
-          <img
-            src="/images/rafiki richard mushagalusa.png"
-            alt=""
-            className="rounded-[50%] w-[3rem]"
-          />
+        <div className="flex justify-center items-center gap-4">
+          <span className="flex items-center text-[#b3b5b7] text-[50px]">
+            <ion-icon name="person-circle-outline"></ion-icon>
+          </span>
+
           <div className="flex flex-col gap-0 text-lg  leading-5">
-            <p className="text-[#b3b5b7] ">Rafiki </p>
-            <p className="text-[#b3b5b7] ">Richard</p>
+            <p className="text-[#b3b5b7] ">{currentUser.username} </p>
+            <p className="text-[#b3b5b7] ">{`ID: ${currentUser.id}`}</p>
           </div>
         </div>
 
